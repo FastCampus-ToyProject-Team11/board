cd7051bf51f05dbce256b0358194ad2e2e123252
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-09 21:01:47 +0900

    feature: Report 테이블 모델링 완료 테이블명: report_tb user_id 와 board_id 컬럼 다중 유니크 제약조건 Issue: #4

---
+++ src/main/java/com/fastcampus/board/report/Report.java
@@ -0,0 +1,38 @@
+package com.fastcampus.board.report;
+
+import com.fastcampus.board.board.Board;
+import com.fastcampus.board.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "report_tb", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"user_id", "board_id"})
+})
+@Entity
+public class Report {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+}
