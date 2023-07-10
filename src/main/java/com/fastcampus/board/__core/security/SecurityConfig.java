3adfeafcf58f706e18af5c7fe57e5488a771ae02
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-10 16:49:39 +0900

    feat: Password Encoder를 Bean으로 등록 (#9) BCryptPasswordEncoder 사용 스프링 시큐리티 필터 체인 설정 변경 /join url에 인증없이 접근 허용

--- src/main/java/com/fastcampus/board/__core/security/SecurityConfig.java
+++ src/main/java/com/fastcampus/board/__core/security/SecurityConfig.java
@@ -3,10 +3,17 @@ package com.fastcampus.board.__core.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -17,7 +24,7 @@ public class SecurityConfig {
 
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**", "/h2-console/**")
+                    .antMatchers("/join", "/auth/**", "/js/**", "/css/**", "/image/**", "/h2-console/**")
                     .permitAll()
                     .anyRequest()
                     .authenticated()
