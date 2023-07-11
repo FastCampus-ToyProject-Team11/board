5b00c1d7fdd1fd6e811b88e3a130f33e7588ad66
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-12 01:25:26 +0900

    feat: 스프링 시큐리티 설정 변경 (#16) formLogin, httpBasic 비활성화 AuthenticationManager, SecurityFilterManager 빈등록

--- src/main/java/com/fastcampus/board/__core/security/SecurityConfig.java
+++ src/main/java/com/fastcampus/board/__core/security/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.fastcampus.board.__core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,25 +19,39 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
 
                 .headers().frameOptions().sameOrigin()
 
-                .and()
-                    .authorizeRequests()
+                .and().formLogin().disable() // x-www-form-urlencoded, UsernamePasswordAuthenticationFilter disable
+
+                .httpBasic().disable() // HttpBasicAuthenticationFilter disable
+
+                .authorizeRequests()
                     .antMatchers("/join", "/auth/**", "/js/**", "/css/**", "/image/**", "/h2-console/**")
                     .permitAll()
                     .anyRequest()
                     .authenticated()
 
-                .and()
-                    .formLogin()
-                    .loginPage("/auth/loginForm")
-                    .loginProcessingUrl("/auth/loginProc")
-                    .defaultSuccessUrl("/");
+                .and().apply(new SecurityFilterManager());
 
         return http.build();
+    }
+
+    public class SecurityFilterManager extends AbstractHttpConfigurer<SecurityFilterManager, HttpSecurity> {
+
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+             AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+            builder.addFilter(new CustomUsernamePasswordAuthenticationFilter(authenticationManager));
+            super.configure(builder);
+        }
     }
 }
