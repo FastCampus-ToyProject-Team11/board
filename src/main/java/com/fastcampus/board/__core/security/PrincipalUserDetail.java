99f9a4d2deda205ea4106072b8f6274d38dabad2
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-12 21:06:39 +0900

    style: UserDetails JavaDoc 생성 (#17)

--- src/main/java/com/fastcampus/board/__core/security/PrincipalUserDetail.java
+++ src/main/java/com/fastcampus/board/__core/security/PrincipalUserDetail.java
@@ -25,31 +25,46 @@ public class PrincipalUserDetail implements UserDetails {
         return user.getUsername();
     }
 
-    // 계정이 만료되지 않았는지를 리턴 (true: 만료되지 않음)
+    /**
+     *
+     * @return 계정 만료 상태 (true: 만료되지 않음)
+     */
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
-    // 계정이 잠겨있지 않는지를 리턴 (true: 잠기지 않음)
+    /**
+     *
+     * @return 계정 잠금 상태 (true: 잠기지 않음)
+     */
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
-    // 비밀번호가 만료되지 않았는지를 리턴 (true: 만료되지 않음)
+    /**
+     *
+     * @return 비밀번호 만료 상태 (true: 만료되지 않음)
+     */
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
-    // 계정이 활성화(사용 가능) 인지를 리턴 (true: 활성화)
+    /**
+     *
+     * @return 계정 활성화(사용 가능) 상태 (true: 활성화)
+     */
     @Override
     public boolean isEnabled() {
         return true;
     }
 
-    // 계정이 가지고 있는 권한 목록을 리턴한다. (권한이 여러개 있을 수 있어서 루프를 돌아야한다.)
+    /**
+     *
+     * @return 계정의 권한 목록
+     */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
 
