1124c145e31abee7ea01fc3dc6e9eafe9c597079
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-12 21:08:51 +0900

    feat: JWT 인증을 통한 로그인 기능 추가 (#17)

--- src/main/java/com/fastcampus/board/user/UserService.java
+++ src/main/java/com/fastcampus/board/user/UserService.java
@@ -1,8 +1,13 @@
 package com.fastcampus.board.user;
 
+import com.fastcampus.board.__core.security.JwtTokenProvider;
+import com.fastcampus.board.__core.security.PrincipalUserDetail;
 import com.fastcampus.board.user.dto.UserRequest;
 import com.fastcampus.board.user.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +18,7 @@ public class UserService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
 
     @Transactional
     public UserResponse.JoinDTO save(UserRequest.JoinDTO joinDTO) throws IllegalArgumentException {
@@ -22,5 +28,20 @@ public class UserService {
 
         User persistenceUser = userRepository.save(user);
         return UserResponse.JoinDTO.from(persistenceUser);
+    }
+
+    public UserResponse.LoginDTOWithJWT login(UserRequest.LoginDTO loginDTO) {
+
+        UsernamePasswordAuthenticationToken token
+                = new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword());
+
+        Authentication authentication = authenticationManager.authenticate(token);
+        PrincipalUserDetail principal = (PrincipalUserDetail) authentication.getPrincipal();
+        final User user = principal.getUser();
+
+        UserResponse.LoginDTO loginResponse = UserResponse.LoginDTO.from(user);
+        String jwt = JwtTokenProvider.create(user);
+
+        return UserResponse.LoginDTOWithJWT.from(loginResponse, jwt);
     }
 }
