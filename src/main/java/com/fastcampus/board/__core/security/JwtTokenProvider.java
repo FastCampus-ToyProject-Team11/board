3ef618bd0387ddb96080fd81d2f67971bb4298f9
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-13 01:42:35 +0900

    refactor: 토큰의 키를 환경변수에 저장하여 노출되지 않도록 설정 (#24)

--- src/main/java/com/fastcampus/board/__core/security/JwtTokenProvider.java
+++ src/main/java/com/fastcampus/board/__core/security/JwtTokenProvider.java
@@ -6,17 +6,31 @@ import com.auth0.jwt.exceptions.SignatureVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fastcampus.board.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Date;
 
+@RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
 
     public static final Long EXP = 1000L * 60 * 60 * 48;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER = "Authorization";
-    public static final String SECRET = "MySecretKey";
+
+    @Value("${secret-key}")
+    private static String secretKey;
+
+    private final Environment environment;
+
+    @PostConstruct
+    private void init() {
+        secretKey = environment.getProperty("secret-key");
+    }
 
     public static String create(User user) {
         String jwt = JWT.create()
@@ -24,13 +38,13 @@ public class JwtTokenProvider {
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXP))
                 .withClaim("id", user.getId())
                 .withClaim("role", user.getRole().name())
-                .sign(Algorithm.HMAC512(SECRET));
+                .sign(Algorithm.HMAC512(secretKey));
 
         return TOKEN_PREFIX + jwt;
     }
 
     public static DecodedJWT verify(String jwt) throws SignatureVerificationException, TokenExpiredException {
-        return JWT.require(Algorithm.HMAC512(SECRET))
+        return JWT.require(Algorithm.HMAC512(secretKey))
                 .build()
                 .verify(jwt);
     }
