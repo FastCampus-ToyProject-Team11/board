9cca37bebc37372ec0ee16f1c0b0d1e87cb802f9
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-12 20:59:19 +0900

    feat: 스프링 시큐리티 필터 Response 클래스 구현 (#21) HttpServletResponse 버퍼에 직접 접근하여 응답을 보내는 방식

---
+++ src/main/java/com/fastcampus/board/__core/util/FilterResponse.java
@@ -0,0 +1,33 @@
+package com.fastcampus.board.__core.util;
+
+import com.fastcampus.board.__core.errors.exception.Exception401;
+import com.fastcampus.board.__core.errors.exception.Exception403;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class FilterResponse {
+
+    private static final ObjectMapper OBJECT_MAPPER;
+
+    static {
+        OBJECT_MAPPER = new ObjectMapper();
+    }
+
+    public static void unAuthorized(HttpServletResponse response, Exception401 exception) throws IOException {
+        response.setStatus(exception.status().value());
+        response.setContentType("application/json; charset=utf-8");
+
+        String responseBody = OBJECT_MAPPER.writeValueAsString(exception.body());
+        response.getWriter().println(responseBody);
+    }
+
+    public static void forbidden(HttpServletResponse response, Exception403 exception) throws IOException {
+        response.setStatus(exception.status().value());
+        response.setContentType("application/json; charset=utf-8");
+
+        String responseBody = OBJECT_MAPPER.writeValueAsString(exception.body());
+        response.getWriter().println(responseBody);
+    }
+}
