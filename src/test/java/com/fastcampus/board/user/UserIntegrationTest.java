f714310f3ffe6b5b88d16d4dbb10a148106f4bb1
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-14 23:39:39 +0900

    fix: 로그인 관련 유저 통합 테스트 코드 삭제 (#39)

--- src/test/java/com/fastcampus/board/user/UserIntegrationTest.java
+++ src/test/java/com/fastcampus/board/user/UserIntegrationTest.java
@@ -5,7 +5,6 @@ import com.fastcampus.board.__core.errors.ErrorMessage;
 import com.fastcampus.board.__core.errors.exception.DuplicateNickNameException;
 import com.fastcampus.board.__core.errors.exception.DuplicateUsernameException;
 import com.fastcampus.board.__core.errors.exception.Exception500;
-import com.fastcampus.board.__core.security.JwtTokenProvider;
 import com.fastcampus.board.user.dto.UserRequest;
 import com.fastcampus.board.user.dto.UserResponse;
 import org.junit.jupiter.api.Assertions;
@@ -75,64 +74,6 @@ public class UserIntegrationTest {
             userService.save(joinDTO);
         } catch (Exception500 exception) {
             Assertions.assertEquals(ErrorMessage.EMPTY_DATA_FOR_USER_JOIN, exception.getMessage());
-        }
-    }
-
-    @DisplayName("로그인 통합 테스트 - 성공")
-    @Test
-    void login_Success_Test() {
-        // Given
-        UserRequest.LoginDTO loginDTO =
-                new UserRequest.LoginDTO("testUser", "1234");
-
-        // When
-        UserResponse.LoginDTOWithJWT response = userService.login(loginDTO);
-
-        UserResponse.LoginDTO responseLoginDTO = response.getLoginDTO();
-        String responseJwt = response.getJwt();
-        String jwt = responseJwt.replace(JwtTokenProvider.TOKEN_PREFIX, "");
-
-        DecodedJWT decodedJWT = JwtTokenProvider.verify(jwt);
-
-        // Then
-        Assertions.assertEquals("testUser", responseLoginDTO.getUsername());
-        Assertions.assertEquals("testUser", responseLoginDTO.getNickName());
-
-        Assertions.assertEquals("testUser", decodedJWT.getClaim("username").asString());
-        Assertions.assertEquals("SESAC", decodedJWT.getClaim("role").asString());
-    }
-
-    @DisplayName("로그인 통합 테스트 - 실패(유저네임 혹은 패스워드 틀림)")
-    @Test
-    void login_Failed_Test_InvalidUsernameOrPassword() {
-        // Given
-        UserRequest.LoginDTO loginDTO =
-                new UserRequest.LoginDTO("InvalidUsername", "1234");
-
-        // When
-        // Then
-        Assertions.assertThrows(InternalAuthenticationServiceException.class,
-                () -> userService.login(loginDTO));
-        try {
-            userService.login(loginDTO);
-        } catch (InternalAuthenticationServiceException exception) {
-            Assertions.assertEquals(ErrorMessage.LOGIN_FAILED, exception.getMessage());
-        }
-    }
-
-    @DisplayName("로그인 통합 테스트 - 실패(비어 있는 DTO)")
-    @Test
-    void login_Failed_Test_EmptyDTO() {
-        // Given
-        UserRequest.LoginDTO loginDTO = null;
-
-        // When
-        // Then
-        Assertions.assertThrows(Exception500.class, () -> userService.login(loginDTO));
-        try {
-            userService.login(loginDTO);
-        } catch (Exception500 exception) {
-            Assertions.assertEquals(ErrorMessage.EMPTY_DATA_FOR_USER_LOGIN, exception.getMessage());
         }
     }
 
