0fdb7c4c88a7c0e9012cea3e5381dce90031ea34
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-14 23:32:57 +0900

    feat: 인증 방식 변경으로 인한 컨트롤러 수정 (#38) 로그인 요청에 대한 핸들링 삭제 디렉토리 이동

--- src/main/java/com/fastcampus/board/user/controller/UserController.java
+++ src/main/java/com/fastcampus/board/user/controller/UserController.java
@@ -1,28 +1,18 @@
-package com.fastcampus.board.user;
+package com.fastcampus.board.user.controller;
 
 import com.fastcampus.board.__core.security.PrincipalUserDetail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class UserController {
 
-    @GetMapping("/user")
-    public String user(HttpServletRequest request, Authentication authentication) {
-        log.info("/user GET jwt: " + request.getHeader("Authorization"));
-        log.info("auth: " + authentication.getPrincipal());
-        return "/user";
-    }
-
-    @GetMapping("/index")
+    @GetMapping({"/index", "/"})
     public String index(@AuthenticationPrincipal PrincipalUserDetail userDetail) {
         log.info("/index GET userDetail:" + userDetail);
         return "/index";
