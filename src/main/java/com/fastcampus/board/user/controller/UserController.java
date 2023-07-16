2ee86cbe29568dac2f2787eb90734cd55a150b23
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-16 21:51:54 +0900

    feature: 회원 수정 폼 생성 (#44)

--- src/main/java/com/fastcampus/board/user/controller/UserController.java
+++ src/main/java/com/fastcampus/board/user/controller/UserController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
@@ -26,5 +27,11 @@ public class UserController {
     @GetMapping("/joinForm")
     public String joinForm() {
         return "/user/joinForm";
+    }
+
+    @GetMapping("/user/userForm")
+    public String userForm(@AuthenticationPrincipal PrincipalUserDetail userDetail, Model model) {
+        model.addAttribute("user", userDetail.getUser());
+        return "/user/userForm";
     }
 }
