29df2e351ca300dc16f7f894d66fbe5c2586ae4f
Miyeon Lee <leeyun92@naver.com>
2023-07-18 15:55:10 +0900

    [#47] feat : BoardController 게시글 목록 조회 메소드 구현

--- src/main/java/com/fastcampus/board/board/controller/BoardController.java
+++ src/main/java/com/fastcampus/board/board/controller/BoardController.java
@@ -1,15 +1,21 @@
 package com.fastcampus.board.board.controller;
 
 import com.fastcampus.board.board.dto.BoardRequest;
+import com.fastcampus.board.board.dto.BoardResponse;
+import com.fastcampus.board.board.model.Role;
 import com.fastcampus.board.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 @RequiredArgsConstructor
 @RequestMapping("/board")
@@ -20,7 +26,7 @@ public class BoardController {
 
     @GetMapping("/save")
     public String saveForm(){
-        return "save";
+        return "board/save";
     }
 
     @PostMapping("/save")
@@ -32,5 +38,49 @@ public class BoardController {
         Long id = boardService.save(saveDTO);
         return "redirect:/board/" + id;
 
+    }
+
+    // /board/list?page=1
+    @GetMapping("/list")
+    public String boardList(@PageableDefault(page=1) Pageable pageable, Model model,
+                            @RequestParam(value = "keyword", required = false) String keyword) {
+
+        Page<BoardResponse.ListDTO> boardList = boardService.findAll(keyword, pageable);
+
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "board/list";
+    }
+
+    @GetMapping("/list/{category}")
+    public String boardListByRole(@PageableDefault(page=1) Pageable pageable, Model model,
+                                  @RequestParam(value = "keyword", required = false) String keyword, @RequestParam Role role) {
+
+        Page<BoardResponse.ListDTO> boardList = boardService.findAllByCategory(role, keyword, pageable);
+
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        return "board/list";
+    }
+
+
+    @ResponseBody
+    @GetMapping("/image/{storedFileName}")
+    public Resource image(@PathVariable String storedFileName) throws MalformedURLException {
+
+        String savePath = boardService.getSavePath(storedFileName);
+        return new UrlResource("file:" + savePath);
     }
 }
