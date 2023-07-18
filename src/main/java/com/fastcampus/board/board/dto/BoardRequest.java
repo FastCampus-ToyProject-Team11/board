3fc26e35887fe6ae613e0508c9aa5348107b2375
Miyeon Lee <leeyun92@naver.com>
2023-07-18 21:04:15 +0900

    [#53] feat : BoardRequest.UpdateDTO 구현

--- src/main/java/com/fastcampus/board/board/dto/BoardRequest.java
+++ src/main/java/com/fastcampus/board/board/dto/BoardRequest.java
@@ -29,4 +29,20 @@ public class BoardRequest {
         private int fileAttached;
 
     }
+
+    @Getter
+    @Setter
+    public static class UpdateDTO {
+
+        private Long id;
+
+        @NotBlank(message = "제목을 입력해주세요.")
+        @Size(max = 100, message = "제목은 100자 이내로 작성주세요.")
+        private String title;
+
+        @NotBlank(message = "내용을 입력해주세요.")
+        @Size(max = 20000, message = "내용은 20,000자 이내로 작성해주세요.")
+        private String content;
+
+    }
 }
