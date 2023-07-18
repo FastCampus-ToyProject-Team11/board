46b237e8de1d6f5a2a33b475a6f08ba9b6c7d806
Miyeon Lee <leeyun92@naver.com>
2023-07-18 21:06:30 +0900

    [#53] feat : 게시글 수정 Service 메소드 구현

--- src/main/java/com/fastcampus/board/board/model/Board.java
+++ src/main/java/com/fastcampus/board/board/model/Board.java
@@ -1,10 +1,7 @@
 package com.fastcampus.board.board.model;
 
 import com.fastcampus.board.user.User;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -17,6 +14,7 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 @Getter
+@Setter
 @Table(name = "board_tb")
 @Entity
 public class Board {
