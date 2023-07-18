1fb7092070a6940f52bfa262f4edc3479722dfa9
Miyeon Lee <leeyun92@naver.com>
2023-07-18 19:21:31 +0900

    [#57] feat : BoardRepository 게시글 목록 보기 구현

--- src/main/java/com/fastcampus/board/board/repository/BoardRepository.java
+++ src/main/java/com/fastcampus/board/board/repository/BoardRepository.java
@@ -1,7 +1,23 @@
 package com.fastcampus.board.board.repository;
 
 import com.fastcampus.board.board.model.Board;
+import com.fastcampus.board.board.model.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+
+//    @Query("SELECT b FROM Board b LEFT JOIN b.user u WHERE (b.title LIKE %:keyword% OR b.content LIKE %:keyword% OR u.nickname LIKE %:keyword%)")
+    @Query("SELECT b FROM Board b LEFT JOIN b.user u WHERE (b.title LIKE %:keyword% OR b.content LIKE %:keyword%)")
+    Page<Board> findAllByKeyword(String keyword, Pageable pageable);
+
+    @Query("SELECT b FROM Board b LEFT JOIN b.user u WHERE b.role = :role")
+    Page<Board> findAllByCategory(Role role, Pageable pageable);
+
+//    @Query("SELECT b FROM Board b LEFT JOIN b.user u WHERE (b.title LIKE %:keyword% OR b.content LIKE %:keyword% OR u.nickname LIKE %:keyword%) AND b.role = :role")
+    @Query("SELECT b FROM Board b LEFT JOIN b.user u WHERE (b.title LIKE %:keyword% OR b.content LIKE %:keyword%) AND b.role = :role")
+    Page<Board> findAllByKeywordCategory(Role role, String keyword, Pageable pageable);
 }
