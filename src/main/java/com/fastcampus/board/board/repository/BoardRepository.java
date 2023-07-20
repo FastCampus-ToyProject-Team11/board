a7a7cf0e52101ab27826298fc225ed745f97de21
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-20 16:37:12 +0900

    feat: 게시글 수가 10개 이상인데 Role이 SESAC인 유저를 검색하는 쿼리 추가 (#85)

--- src/main/java/com/fastcampus/board/board/repository/BoardRepository.java
+++ src/main/java/com/fastcampus/board/board/repository/BoardRepository.java
@@ -1,11 +1,14 @@
 package com.fastcampus.board.board.repository;
 
+import com.fastcampus.board.board.dto.BoardResponse;
 import com.fastcampus.board.board.model.Board;
 import com.fastcampus.board.user.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
@@ -18,4 +21,11 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Query("SELECT b FROM Board b LEFT JOIN b.user u WHERE (b.title LIKE %:keyword% OR b.content LIKE %:keyword% OR u.nickName LIKE %:keyword%) AND b.role = :role")
     Page<Board> findAllByKeywordRole(Role role, String keyword, Pageable pageable);
+
+    @Query("SELECT new com.fastcampus.board.board.dto.BoardResponse$UserSummary(b.user.id, b.user.role, COUNT(b)) " +
+            "FROM Board b " +
+            "WHERE b.user.role = 'SESAC' " +
+            "GROUP BY b.user.id, b.user.role " +
+            "HAVING COUNT(b) >= 10")
+    List<BoardResponse.UserSummary> getUserSummary();
 }
